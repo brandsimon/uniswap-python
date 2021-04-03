e588302fe81185d6b71bf99dff2cef528165e631
Simon Brand <simon.brand@postadigitale.de>
2021-04-03 22:38:59 +0000

    safe_venv.sh: add ccxt

--- safe_venv.sh
+++ safe_venv.sh
@@ -5,8 +5,10 @@ bwrap \
         --ro-bind /lib /lib  \
         --ro-bind /lib64 /lib64 \
         --ro-bind "/home/admin/uniswap-python" "/home/admin/uniswap-python" \
+        --ro-bind "/home/admin/ccxt/python" "/home/admin/ccxt/python" \
         --bind "/home/admin/uniswap-python/.insecure_venv/" "/home/admin/uniswap-python/.insecure_venv/" \
         --bind "/home/admin/uniswap-python/uniswap.egg-info/" "/home/admin/uniswap-python/uniswap.egg-info/" \
+        --bind "/home/admin/uniswap-python/_ccxt.egg-info/" "/home/admin/ccxt/python/ccxt.egg-info/" \
         --dev /dev \
         --tmpfs /tmp \
         --unshare-user \
